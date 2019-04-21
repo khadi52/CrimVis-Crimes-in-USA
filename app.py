3a695e6f86c8a25757d61573fa3d2ce697fc5351
khadi52 <khadijachowdhry52@gmail.com>
2019-04-21 17:01:09 -0400

    Adding CrimeVis Visualization project

---
+++ app.py
@@ -0,0 +1,200 @@
+import json
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.manifold import MDS
+from sklearn.metrics import pairwise_distances
+from numpy import array
+from sklearn import preprocessing
+
+from flask import Flask, render_template, request, redirect, Response, jsonify
+
+#First of all you have to import it from the flask module:
+app = Flask(__name__)
+#By default, a route only answers to GET requests. You can use the methods argument of the route() decorator to handle different HTTP methods.
+@app.route("/", methods = ['POST', 'GET'])
+def index():
+    #return the original cleaned dataframe to plot the dashboard
+    if request.method == 'POST':
+        if request.form['data'] == 'received':
+             chart_data = state_df.to_dict(orient='records')
+             chart_data = json.dumps(chart_data, indent=2)
+             data = {'chart_data': chart_data}
+             return jsonify(data)
+    return render_template("index.html")
+
+#Task 3 Part 1 - Plots the 2D scatter plot for all 3 dataset of top 2 PCA vectors
+@app.route("/pca_scatter", methods = ['POST', 'GET'])
+def pca_scatter():
+    global stratified_sampled_data
+    global scaled_df
+    global random_sampled_data
+
+    pca = PCA(n_components = 2)
+
+    if request.method == 'POST':
+        if request.form['data'] == 'random':
+            pca_transform = pca.fit_transform(random_sampled_data)
+            data = pd.DataFrame(pca_transform, columns = ['component1','component2'])
+            chart_data = data.to_dict(orient='records')
+            chart_data = json.dumps(chart_data, indent=2)
+            data = {'chart_data': chart_data}
+            return jsonify(data) # Should be a json string
+        elif request.form['data'] == 'stratified':
+                pca_transform = pca.fit_transform(stratified_sampled_data)
+                data = pd.DataFrame(pca_transform, columns = ['component1','component2'])
+                chart_data = data.to_dict(orient='records')
+                chart_data = json.dumps(chart_data, indent=2)
+                data = {'chart_data': chart_data}
+                return jsonify(data) # Should be a json string
+        else:
+            pca_transform = pca.fit_transform(scaled_df)
+            data = pd.DataFrame(pca_transform, columns=['component1', 'component2'])
+            chart_data = data.to_dict(orient='records')
+            chart_data = json.dumps(chart_data, indent=2)
+            data = {'chart_data': chart_data}
+            return jsonify(data)  # Should be a json string
+
+@app.route("/pca", methods = ['POST', 'GET'])
+def pca():
+    global variance
+    global sum_sqaured_loading
+    global scaled_df
+    global features_list
+
+    print(sum_sqaured_loading)
+    print(features_list)
+    indices = list(range(1, 13))
+    zip_list = list(zip(indices, variance,sum_sqaured_loading,features_list))
+
+    data = pd.DataFrame(zip_list, columns = ['pca_component' , 'eigen_values','sum_sqaured_loadings','features_name'])
+
+    if request.method == 'POST':
+        if request.form['data'] == 'received':
+             chart_data = data.to_dict(orient='records')
+             chart_data = json.dumps(chart_data, indent=2)
+             data = {'chart_data': chart_data}
+             return jsonify(data)
+    return ""
+
+#Task 3 Part 3 - Scatter Matrix Plot for random, stratified and original dataset
+@app.route("/scatter_matrix", methods = ['POST', 'GET'])
+def scatter_matrix():
+    global features_list
+    global top_features_original
+    global top_features_random
+    global top_features_stratified
+    global stratified_sampled_data
+    global scaled_df
+    global random_sampled_data
+
+    if request.method == 'POST':
+        if request.form['data'] == 'random':
+            data = pd.DataFrame()
+            data[features_list[top_features_random[0]-1]] = random_sampled_data[features_list[top_features_random[0]-1]]
+            data[features_list[top_features_random[1]-1]] = random_sampled_data[features_list[top_features_random[1]-1]]
+            data[features_list[top_features_random[2]-1]] = random_sampled_data[features_list[top_features_random[2]-1]]
+            chart_data = data.to_dict(orient='records')
+            chart_data = json.dumps(chart_data, indent=2)
+            data = {'chart_data': chart_data}
+            return jsonify(data)
+        elif request.form['data'] == 'stratified':
+            data = pd.DataFrame()
+            data[features_list[top_features_stratified[0]-1]] = stratified_sampled_data[features_list[top_features_stratified[0]-1]]
+            data[features_list[top_features_stratified[1]-1]] = stratified_sampled_data[features_list[top_features_stratified[1]-1]]
+            data[features_list[top_features_stratified[2]-1]] = stratified_sampled_data[features_list[top_features_stratified[2]-1]]
+            chart_data = data.to_dict(orient='records')
+            chart_data = json.dumps(chart_data, indent=2)
+            data = {'chart_data': chart_data}
+            return jsonify(data)
+        else:
+            data = pd.DataFrame()
+            data[features_list[top_features_original[0]-1]] = scaled_df[features_list[top_features_original[0]-1]]
+            data[features_list[top_features_original[1]-1]] = scaled_df[features_list[top_features_original[1]-1]]
+            data[features_list[top_features_original[2]-1]] = scaled_df[features_list[top_features_original[2]-1]]
+            chart_data = data.to_dict(orient='records')
+            chart_data = json.dumps(chart_data, indent=2)
+            data = {'chart_data': chart_data}
+            return jsonify(data)
+
+#returns eigen values and loadings
+def get_pca_parameters(data):
+    pca = PCA()
+    pca.fit_transform(data)
+    return pca.explained_variance_ , pca.components_
+
+#cleans the dataset for PCA
+def clean_dataset():
+    global df_pca
+
+    df_pca.drop('State', axis = 1, inplace=True)
+    df_pca.drop('Area', axis = 1, inplace=True)
+    df_pca.drop('code', axis = 1, inplace=True)
+
+#method to normalise the data using MaxMinScaler
+def get_scaled_data():
+    global scaled_df
+    global df_pca
+    # Scaling the data to perform PCA USing MinMaxScaler to scale all features in range of 0-1
+    columns = df_pca.columns
+    print(columns)
+    scaler = StandardScaler()
+    scaled_df = scaler.fit_transform(df_pca)
+    scaled_df = pd.DataFrame(scaled_df, columns=columns)
+
+#TASK 2 - Computing squared sum loadings to find three attributes with highest loaded PCA attributes
+def get_sqaured_sum_loadings(data,k):
+    features = len(data)
+    sqaured_loadings = []
+    for pci in range(0,features):
+        sq_load = 0
+        for j in range(0,k):
+            sq_load += data[j][pci] * data[j][pci]
+        sqaured_loadings.append(sq_load)
+    return sqaured_loadings
+
+#TASK 2 - returns top attributes with higest squared sum loadings
+def print_table_loadings(loading,sum_sqaured_loading):
+    features = range(1,len(sum_sqaured_loading)+1)
+    a = array(features)
+    b = array(loading)
+    c = array(sum_sqaured_loading)
+    result = array(list(zip(a,b[0,:],b[1,:],b[2,:],b[3,:],c)))
+    result_list = result.tolist()
+    result_list = sorted(result_list, key=lambda l: l[5], reverse=True)
+
+    print('Table to show sum of squared loadings\n')
+    for row in result_list:
+        print(' '.join([str(elem) for elem in row]))
+    return [int(i[0]) for i in result_list]
+
+if __name__ == "__main__":
+    global state_df, scaled_df, df_pca
+    orig_data = pd.read_csv('data/crime-dataset.csv')
+    df_pca = orig_data.copy(deep=True)
+
+    state_df = orig_data.groupby(['Year', 'id', 'State', 'State Population'])["Total Crimes", "Violent Crime"].apply(
+        lambda x: x.astype(int).sum()).reset_index()
+    state_df = state_df.loc[state_df.Year == 2017]
+
+    clean_dataset()
+    #dataframe to store normalized data
+    scaled_df = pd.DataFrame()
+
+    get_scaled_data()
+
+    variance, loading  = get_pca_parameters(scaled_df)
+    #Finding Sum of Squared Loadings to get the 3 attributes with highest PCA loadings for stratified sampled data
+    sum_sqaured_loading = get_sqaured_sum_loadings(loading,len(loading))
+    print(sum_sqaured_loading)
+    #printing table of top attributes with highest loadings
+    top_features = print_table_loadings(loading,sum_sqaured_loading)
+
+    features_list = list(scaled_df.columns.values)
+    print("Top 3 features with highest PCA loadings - ", features_list[top_features[0]-1], ', ',features_list[top_features[1]-1], ', ',features_list[top_features[2]-1])
+
+    app.run(debug=True)
---
+++ static/js/plots.js
@@ -0,0 +1,310 @@
+const pca_view_opt = d3.select(".pca_option");
+console.log(pca_view_opt);
+const dash_view_opt = d3.select(".dash_option");
+const PCA = "PCA_ANALYSIS";
+const DASHBOARD = "DASHBOARD"
+var path = d3.geoPath();
+var color = {}, stateNames = {}, violentCrime = {}, totalCrime = {};
+const margin = {top: 30, right: 20, bottom: 30, left: 50};
+
+function route(view) {
+    console.log(view)
+    if(view == PCA){
+        $.post("pca", {'data': 'received'}, plot_scree_plot);
+    }
+    else if (view == DASHBOARD){
+        $.post("", {'data': 'received'}, plot_dashboard);
+    }
+    else if(view == "PCA Scatter Stratified Sampled Data"){
+        $.post("pca_scatter", {'data': 'stratified'}, function(data) {
+            plot_scatter_plot(data,"2D Scatter Plot of Projection of Stratified Sampled Data on top 2 PCA Vectors");
+        })
+    }
+    else if(view == "PCA Scatter Original Sampled Data"){
+        $.post("pca_scatter", {'data': 'original'}, function(data) {
+            plot_scatter_plot(data,"2D Scatter Plot of Projection of Original Sampled Data on top 2 PCA Vectors");
+        })
+    }
+    else if(view == "MDS Euclidean Scatter Stratified Sampled Data"){
+        $.post("mds_euclidean", {'data': 'stratified'}, function(data) {
+            plot_scatter_plot(data,"2D Scatter Plot of MDS Euclidean on Stratified Sampled Data");
+        })
+    }
+    else if(view == "MDS Euclidean Scatter Original Sampled Data"){
+        $.post("mds_euclidean", {'data': 'original'}, function(data) {
+            plot_scatter_plot(data,"2D Scatter Plot of MDS Euclidean on Original Sampled Data");
+        })
+    }
+    else if(view == "MDS Euclidean Scatter Random Sampled Data"){
+        $.post("mds_euclidean", {'data': 'random'}, function(data) {
+            plot_scatter_plot(data,"2D Scatter Plot of MDS Euclidean on Random Sampled Data");
+        })
+    }
+    else if(view == "MDS Correlation Scatter Stratified Sampled Data"){
+        $.post("mds_correlation", {'data': 'stratified'}, function(data) {
+            plot_scatter_plot(data,"2D Scatter Plot of MDS Correlation on Stratified Sampled Data");
+        })
+    }
+    else if(view == "MDS Correlation Scatter Original Sampled Data"){
+        $.post("mds_correlation", {'data': 'original'}, function(data) {
+            plot_scatter_plot(data,"2D Scatter Plot of MDS Correlation on Original Sampled Data");
+        })
+    }
+    else if(view == "MDS Correlation Scatter Random Sampled Data"){
+        $.post("mds_correlation", {'data': 'random'}, function(data) {
+            plot_scatter_plot(data,"2D Scatter Plot of MDS Correlation on Random Sampled Data");
+        })
+    }
+    else if(view == "Scatter Matrix Plot Stratified Sampled Data"){
+        $.post("scatter_matrix", {'data': 'stratified'}, function(data) {
+            plot_scatter_matrix(data,"Scatter Matrix Plot of 3 highest PCA loaded attributes on Stratified Sampled Data");
+        })
+    }
+    else if(view == "Scatter Matrix Plot Original Data"){
+        $.post("scatter_matrix", {'data': 'original'}, function(data) {
+            plot_scatter_matrix(data,"Scatter Matrix Plot of 3 highest PCA loaded attributes on Original Data");
+        })
+    }
+    else if(view == "Scatter Matrix Plot Random Sampled Data"){
+        $.post("scatter_matrix", {'data': 'random'}, function(data) {
+            plot_scatter_matrix(data,"Scatter Matrix Plot of 3 highest PCA loaded attributes on Random Sampled Data");
+        })
+    }
+}
+
+function plot_dashboard(data){
+	draw_all_crimes(data);
+}
+
+function tooltipHtml(d){   /* function to create html content string in tooltip div. */
+    //d is the id of the state.. get the row which has this id
+    d.id = +d.id;
+    return "<h4>"+stateNames[d.id]+"</h4><table>"+
+        "<tr><td>Violent Crimes</td><td>"+(violentCrime[d.id])+"</td></tr>"+
+        "<tr><td>Total Crimes</td><td>"+(totalCrime[d.id])+"</td></tr>"+
+        // "<tr><td>High</td><td>"+(d.high)+"</td></tr>"+
+        "</table>";
+}
+
+function draw_all_crimes(data) {
+    d3.select(".chart-container").remove();
+    data = JSON.parse(data.chart_data);
+    d3.select(".container-fluid")
+        .append("div")
+        .attr("class","chart-container")
+        .append("div")
+        .attr("class","flex-container")
+        .append("div")
+        .attr("class","first-box")
+        .attr("id", "usmap-box");
+
+    d3.select(".flex-container")
+        .append("div")
+        .attr("class","first-box second-box");
+
+    d3.select(".chart-container")
+        .append("div")
+        .attr("class","flex-container")
+        .attr("id", "second-container")
+        .append("div")
+        .attr("class","first-box");
+
+    d3.select("#second-container")
+        .append("div")
+        .attr("class","first-box second-box");
+
+
+    var first_box_boundary = d3.select(".first-box").node().getBoundingClientRect();
+    console.log(first_box_boundary.height)
+
+    var width = first_box_boundary.width - (2*margin.left) - (2*margin.right),
+        height = first_box_boundary.height - (2*margin.top) - (2*margin.bottom);
+
+
+
+    d3.json("https://d3js.org/us-10m.v1.json", function(error, unitedStates) {
+        if (error) throw error;
+        var us_map = topojson.feature(unitedStates, unitedStates.objects.states).features;
+        var svg = d3.select("#usmap-box").append('div', "usmap")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height +margin.top + margin.bottom);
+        data.forEach(function (d, i) {
+            stateNames[d.id] = d.State;
+            d["Total Crimes"] = +d["Total Crimes"];
+            d["Violent Crime"] = +d["Violent Crime"];
+            violentCrime[d.id] = d['Violent Crime'];
+            totalCrime[d.id] = d['Total Crimes'];
+            //color2017[d.id] = {color:d3.interpolate("#ffffcc", "#800026")(d["Total Crimes"]/10000)}
+        });
+        console.log(data);
+        var pathSvg = svg.append("g")
+            .attr("class", "states")
+            .selectAll("path")
+            .data(us_map)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .on("mouseover", function(d,i) {
+                d3.select("#tooltip").html(tooltipHtml(d))
+                    .style('color', 'black').style('font-size', '10.5px').style('text-align', 'center')
+                    .style('display', 'block').style("left", d3.event.pageX + "px")
+                    .style("top", d3.event.pageY + "px").style("opacity", 1);
+            })
+            .on('mousemove', function(d,i) {
+                d3.select("#tooltip").style('left',(d3.event.layerX - 20)+'px').style('top',(d3.event.layerY + 15)+'px');
+            })
+            .on("mouseout", function() {
+                d3.select(this).transition()
+                    .duration(400);
+                d3.select("#tooltip").style('opacity',0).style('display', 'none');
+            })
+            .style("fill", function(d){ return {color:d3.interpolateRgb("#ffffcc", "#800026")(d["Total Crimes"]/10000)}});
+        svg.append("path")
+            .attr("class", "state-borders")
+            .attr("d", path(topojson.mesh(unitedStates, unitedStates.objects.states, function (a, b) {
+                return a !== b;
+            })))
+
+        //adding tooltip
+    });
+}
+
+//plots scree plot
+function  plot_scree_plot(data) {
+    data = JSON.parse(data.chart_data);
+    console.log(data);
+    data.forEach(function(d) {
+        d.eigen_values = d.eigen_values;
+        d.pca_component = d.pca_component;
+        d.sum_sqaured_loadings = d.sum_sqaured_loadings;
+        d.features_name = d.features_name;
+    });
+
+    d3.select(".chart-container").remove();
+
+    // Set the dimensions of the canvas / graph
+
+    var markIDX = 0;
+    var markIDY = 0;
+
+    d3.select(".container-fluid")
+        .append("div")
+        .attr("class","chart-container")
+        .append("div")
+        .attr("class","flex-container1")
+        .append("div")
+        .attr("class","first-box");
+
+    d3.select(".flex-container1")
+        .append("div")
+        .attr("class","first-box second-box");
+
+    var first_box_boundary = d3.select(".first-box").node().getBoundingClientRect();
+    console.log(first_box_boundary.height);
+
+    var width = first_box_boundary.width - (2*margin.left) - (2*margin.right),
+        height = first_box_boundary.height - (2*margin.top) - (2*margin.bottom);
+
+    // Set the ranges
+    var x = d3.scaleLinear().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+    // Define the axes
+    var xAxis = d3.axisBottom(x).ticks(data.length);
+    var yAxis = d3.axisLeft(y).ticks(10);
+
+    // Define the line
+    var value_line = d3.line()
+        .x(function(d,i) {
+            if (i == 2) {
+                markIDX = x(d.pca_component);
+                markIDY = y(d.eigen_values)
+            }
+            return x(d.pca_component); })
+        .y(function(d) { return y(d.eigen_values); });
+
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d) { return d.pca_component-1; }));
+    y.domain([0, d3.max(data, function(d) {
+        return d.eigen_values;  })]);
+
+    var svg_scree = d3.select(".first-box")
+        .append("div")
+        .attr("id","chart_scree")
+        .attr("class","scree-container")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + 1.5*margin.top + 1.5*margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    svg_scree.append("g")
+        .attr("class", "xaxes")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "xaxisl")
+        .attr("text-anchor", "middle")
+        .attr("y", margin.left)
+        .attr("x", (width / 2))
+        .text("PCA Components");;
+
+    // Add the Y Axis
+    svg_scree.append("g")
+        .attr("class", "yaxes")
+        .call(yAxis)
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .text("Eigen Values");
+
+    svg_scree.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 0 - (margin.top / 2))
+        .style("font-size", "12px")
+        .attr("text-anchor", "middle")
+        .style("text-decoration", "underline")
+        .text("Scree Plot for Dataset");
+
+    // Add the valueline path.
+    svg_scree.append("path")
+        .attr("class", "line")
+        .attr("d", value_line(data));
+
+
+    svg_scree.append("circle")
+        .attr("class","marker")
+        .attr("r", 9)
+        .attr("cx", markIDX)
+        .attr("cy", markIDY)
+        .style("fill", "red");
+
+    svg_scree.selectAll(".point")
+        .data(data)
+        .enter().append("circle")
+        .attr("class", "point")
+        .attr("r", 3)
+        .attr("cx", function(d, i) { return x(d.pca_component) })
+        .attr("cy", function(d) { return y(d.eigen_values) });
+
+
+}
+
+pca_view_opt.on('click', function(){
+    var event = d3.event;
+    console.log("In PCA view");
+    event.preventDefault();
+    route(PCA);
+});
+dash_view_opt.on('click', function(){
+    var event = d3.event;
+    console.log("In Dashboard view");
+    event.preventDefault();
+    route(DASHBOARD);
+});
